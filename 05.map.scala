66ec185b787f732f7eddc288ae15e0db2782a38e
sdia <seydou.dia@olx.com>
2017-06-26 16:56:25 +0200

    add tuples

---
+++ 05.map.scala
@@ -0,0 +1,29 @@
+import scala.io.StdIn.{readLine, readInt}
+import scala.math._
+import scala.collection.mutable.ArrayBuffer
+import java.io.PrintWriter
+import scala.io.Source
+
+
+object ScalaTutorial {
+    def main(args:Array[String]){
+
+        val employee = Map("Manager" -> "Bob Smith",
+                            "Secretary" -> "Alex Brown")
+
+        // map is immutable by default
+        if(employee.contains("Manager"))
+            printf("Manager : %s\n", employee("Manager"))
+
+        println("mutable map")
+        val customers = collection.mutable.Map(100 -> "Paul", 101 -> "Sally")
+
+        printf("Cust 1 : %s\n", customers(100))
+        customers(100) = "Tom Marks"
+        customers(102) = "Megan Swift"
+        for((k,v) <- customers)
+            printf("%d : %s\n", k, v)
+
+
+    }
+}
---
+++ 06.tuples.scala
@@ -0,0 +1,29 @@
+import scala.io.StdIn.{readLine, readInt}
+import scala.math._
+import scala.collection.mutable.ArrayBuffer
+import java.io.PrintWriter
+import scala.io.Source
+
+
+
+object ScalaTutorial {
+    def main(args: Array[String]) {
+
+        println("Tuples hold value of various types, they are immutable.")
+
+        var tupleMarge = (103, "Marge Simpson", 10.25)
+        printf("%s owes us $%.2f\n", tupleMarge._2, tupleMarge._3)
+
+        // __repr__ ?
+        println(tupleMarge.toString())
+
+        // iter
+        tupleMarge.productIterator.foreach(println)
+        tupleMarge.productIterator.foreach{i => println(i)}
+
+
+
+    }
+
+
+}
